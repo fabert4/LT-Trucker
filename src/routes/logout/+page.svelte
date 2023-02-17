7fdb79d7e532a62a7d94e2b1c57dd3541ce01590
T. Faber <tijn.faber@hva.nlgit>
2023-02-17 12:49:40 +0100

    - Fix

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,13 +1,13 @@
-<!--<script lang="ts">-->
-<!--    import { goto } from '$app/navigation';-->
-<!--    import { page } from '$app/stores';-->
-<!--    import { onMount } from 'svelte';-->
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
+    import { onMount } from 'svelte';
 
-<!--    onMount(async () => {-->
-<!--        await fetch('/api/logout', {-->
-<!--            method: 'POST'-->
-<!--        });-->
+    onMount(async () => {
+        await fetch('/api/logout', {
+            method: 'POST'
+        });
 
-<!--        goto(`/login${$page.url.search}`);-->
-<!--    });-->
-<!--</script>-->
+        goto(`/login${$page.url.search}`);
+    });
+</script>
